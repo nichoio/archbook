31b8b5ac7606f79be8a3402b00ed15d1706ca44c
Nicho Kappel <you@example.com>
2021-04-02 17:39:41 +0200

    fish grep no binary

--- fish/functions/grep.fish
+++ fish/functions/grep.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function grep --wraps='grep -in --color' --description 'alias grep=grep -i --color'
- command grep -in --color $argv;
+ command grep -iIn --color $argv;
 end
