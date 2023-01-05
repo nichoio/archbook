2ffa7a93b03be81fdc22e19590e26bfe4c0921cd
Nicholas Kappel <nicholas.kappel@alexanderthamm.com>
2023-01-05 18:38:51 +0100

    add helpu

--- zsh/nichoio/nichoio.plugin.zsh
+++ zsh/nichoio/nichoio.plugin.zsh
@@ -7,9 +7,30 @@ alias kally='(){kubectl api-resources --verbs=list --namespaced -o name  | xargs
 alias kalln='(){kubectl api-resources --verbs=list --namespaced -o name  | xargs -n 1 kubectl get --show-kind --ignore-not-found  -n $1}'
 alias kallny='(){kubectl api-resources --verbs=list --namespaced -o name  | xargs -n 1 kubectl get --show-kind --ignore-not-found  -n $1 -o yaml}'
 
+function helpu() {  # Unpacks given Helm chart to a new folder under current directory.
+    if [ "$1" = "" ]
+    then
+      echo "must provide at least one argument: CHART-URL [CHART-NAME]"
+      return 1
+    fi
+
+    # TODO: add ability to add --version
+    helm repo add somechart $1
+
+    if [ "$2" != "" ]
+    then
+        helm repo update && helm pull somechart/$2 --untar
+    else
+        helm repo update && helm pull somechart --untar
+    fi
+
+    helm repo remove somechart
+}
+
 alias tf='terraform'
 
 # extend omz lib's grep alias
-alias grep='grep -in --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.terraform}'
+# grep + ignore several dirs. grep can still be applied to excluded dirs manually
+alias grep='grep -in --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.terraform,test,tests}'
 alias cgrep='grep -C 2'
 alias ccgrep='grep -C 5'
