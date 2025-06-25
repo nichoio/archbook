88371ea2d29c54089752318104603a31d5be5f24
Nicholas Kappel <work@nicho.io>
2025-06-25 15:56:41 +0200

    change nvim tabs from 4 to 2 spaces

--- nvim/macos/init.lua
+++ nvim/macos/init.lua
@@ -18,8 +18,8 @@ vim.o.background = "dark" -- or "light" for light mode
 
 -- Indent by using spaces instead of tabs
 vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 -- Editor settings (opt)
 vim.opt.ignorecase = true
